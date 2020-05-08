3d02515c065466ec36417a8bb8caeb5d9e96f5b1
randy gonzales <randy.gonzales@gmail.com>
2020-05-08 18:40:38 +0800

    unlearn

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Kilroy was here!
-        </p>
+        <p>Kilroy was here!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn AWS Amplify
+          Unlearn everything
         </a>
       </header>
     </div>
